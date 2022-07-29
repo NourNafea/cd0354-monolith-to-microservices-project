ab6e23450c1ac406e98842b0eaf9c88a4e311912
NourNafea <nour68.nafea@gmail.com>
2022-07-29 14:37:59 +0200

    Edit endpoint

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a626b587b04f545dab3c77eab91f144e-1271021496.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
